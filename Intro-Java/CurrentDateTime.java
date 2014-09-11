ee89664b51a82c78fde4e354aaab42bd5c15211f
abanderov <a_banderov@yahoo.com>
2014-09-12 00:06:14 +0300

    from first homework

---
+++ Intro-Java/CurrentDateTime.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class CurrentDateTime {
+	public static void main(String[] args) {
+		Date today = new Date();
+		System.out.println(today);
+	}
+
+}
---
+++ Intro-Java/PrintHometown.java
@@ -0,0 +1,6 @@
+
+public class PrintHometown {
+ public static void main(String[] args) {
+	System.out.println("My home town is Sofia");
+ 	}
+}
